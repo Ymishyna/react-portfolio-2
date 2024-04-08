a6130091988c872cfa1e13a6fe54054b20459f7f
Yana Mishyna <yana.mishina.92@gmail.com>
2024-04-08 14:07:40 -0400

    finisxhed conntact me with all proper links

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,13 +3,14 @@ import './index.scss';
 import logoY from '../../assets/images/logo-y.png';
 // import LogoS from '../../assets/images/logo-s.png';
 // import LogoSubtitle from '../../assets/images/logo_sub.png';
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faHome, faMailForward, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
     <div className="nav-bar">
         <Link className="logo" to="/">
-            <img src={logoY} alt="logo" />  
+            <img src={logoY} alt="logo" />
             {/* <img src={LogoS} alt="logo" />
             <img className="sub-logo" src={LogoSubtitle} alt="mishyna" /> */}
         </Link>
@@ -24,6 +25,23 @@ const Sidebar = () => (
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/yana-mishyna/'>
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://github.com/Ymishyna'>
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='mailto:yana.mishina.92@gmail.com'>
+                    <FontAwesomeIcon icon={faMailForward} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
 
     </div>)
 
