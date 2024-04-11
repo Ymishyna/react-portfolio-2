b16dc8fe10f0e04ac9d45487b640fa8db964f01d
Yana Mishyna <yana.mishina.92@gmail.com>
2024-04-11 12:06:38 -0400

    started creating Contac t componenet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { Route, Routes } from 'react-router';
-import Layout from './components/Layout';
-import Home from './components/Home';
+import Home from './components/Home'
 import About from './components/About'
+import Contact from './components/Contact'
+import Layout from './components/Layout'
 import './App.scss';
 
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Route>
       </Routes>
     </>
