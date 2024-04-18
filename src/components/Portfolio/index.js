8d62e29e92b94e04dae3b9a8514ca3dfa2c0252f
Yana Mishyna <yana.mishina.92@gmail.com>
2024-04-18 15:37:48 -0400

    created Portfolio component, added styling

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -18,23 +18,29 @@ const Portfolio = () => {
 
     const renderPortfolio = (portfolio) => {
         return (
-            <div className='images-container'>
+            <div className="images-container">
                 {
                     portfolio.map((port, idx) => {
                         return (
-                            <div className='image-box' key={idx}>
+                            <div className="image-box" key={idx}>
                                 <img
                                     src={port.cover}
-                                    className='portfolio-image'
-                                    alt='portfolio' />
-
+                                    className="portfolio-image"
+                                    alt="portfolio" />
+                                <div className="content">
+                                    <p className="title">{port.title}</p>
+                                    <h4 className="description">{port.description}</h4>
+                                    <button
+                                        className="btn"
+                                        onClick={() => window.open(port.url)}
+                                    >View</button>
+                                </div>
                             </div>
                         )
                     })
                 }
-
             </div>
-        )
+        );
     }
 
     return (
