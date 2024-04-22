80e2a5ea81ed46d0d51b3e93f2a50ed33a4f3fec
Yana Mishyna <yana.mishina.92@gmail.com>
2024-04-22 13:40:47 -0400

    added mobile styling to layout, sidebar and logo

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -15,27 +15,28 @@ const Sidebar = () => {
                 <img src={logoY} alt="logo" />
             </Link>
             <nav className={showNav ? 'mobile-show' : ''}>
-                <NavLink exact="true" activeclassname="active" to="/">
+                <NavLink onClick={() => setShowNav(false)} exact="true" activeclassname="active" to="/">
                     <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
                 </NavLink>
-                <NavLink activeclassname="active" className="about-link" to="/about">
+                <NavLink onClick={() => setShowNav(false)} activeclassname="active" className="about-link" to="/about">
                     <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
                 </NavLink>
-                <NavLink activeclassname="active" className="portfolio-link" to="/portfolio">
+                <NavLink onClick={() => setShowNav(false)} activeclassname="active" className="portfolio-link" to="/portfolio">
                     <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
                 </NavLink>
                 <NavLink
+                    onClick={() => setShowNav(false)}
                     activeclassname="active"
                     className="contact-link"
                     to="/contact">
                     <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
                 </NavLink>
                 <FontAwesomeIcon
-                onClick={() => setShowNav(false)}
-                icon={faClose}
-                color='#ffd700'
-                size='3x'
-                className='close-icon'
+                    onClick={() => setShowNav(false)}
+                    icon={faClose}
+                    color='#ffd700'
+                    size='3x'
+                    className='close-icon'
                 />
 
             </nav>
