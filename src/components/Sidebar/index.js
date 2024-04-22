e18ad6c574cddaf643c412c711cd26c6b91c85b1
Yana Mishyna <yana.mishina.92@gmail.com>
2024-04-22 12:43:11 -0400

    fixed sidebar layout bug

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,60 +3,69 @@ import { Link, NavLink } from 'react-router-dom';
 import logoY from '../../assets/images/logo-y.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faHome, faUser, faEnvelope, faSuitcase } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faUser, faEnvelope, faSuitcase, faBars } from '@fortawesome/free-solid-svg-icons';
 
-const Sidebar = () => (
-    <div className="nav-bar">
-        <Link className="logo" to="/">
-            <img src={logoY} alt="logo" />
-        </Link>
-        <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
-                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-            </NavLink>
-            <NavLink activeclassname="active" className="about-link" to="/about">
-                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-            </NavLink>
-            <NavLink activeclassname="active" className="portfolio-link" to="/portfolio">
-                <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
-            </NavLink>
-            <NavLink
-                activeclassname="active"
-                className="contact-link"
-                to="/contact">
-                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-            </NavLink>
+const Sidebar = () => {
 
-        </nav>
-        <ul>
-            <li>
-                <a
-                    href="https://www.linkedin.com/in/yana-mishyna/"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <FontAwesomeIcon
-                        icon={faLinkedin}
-                        color="#4d4d4e"
-                        className="anchor-icon"
-                    />
-                </a>
-            </li>
-            <li>
-                <a
-                    href="https://github.com/Ymishyna"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <FontAwesomeIcon
-                        icon={faGithub}
-                        color="#4d4d4e"
-                        className="anchor-icon"
-                    />
-                </a>
-            </li>
-        </ul>
-    </div>
-);
+    return (
+        <div className="nav-bar">
+            <Link className="logo" to="/">
+                <img src={logoY} alt="logo" />
+            </Link>
+            <nav>
+                <NavLink exact="true" activeclassname="active" to="/">
+                    <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+                </NavLink>
+                <NavLink activeclassname="active" className="about-link" to="/about">
+                    <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+                </NavLink>
+                <NavLink activeclassname="active" className="portfolio-link" to="/portfolio">
+                    <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+                </NavLink>
+                <NavLink
+                    activeclassname="active"
+                    className="contact-link"
+                    to="/contact">
+                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+                </NavLink>
+
+            </nav>
+            <ul>
+                <li>
+                    <a
+                        href="https://www.linkedin.com/in/yana-mishyna/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <FontAwesomeIcon
+                            icon={faLinkedin}
+                            color="#4d4d4e"
+                            className="anchor-icon"
+                        />
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://github.com/Ymishyna"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <FontAwesomeIcon
+                            icon={faGithub}
+                            color="#4d4d4e"
+                            className="anchor-icon"
+                        />
+                    </a>
+                </li>
+            </ul>
+            <FontAwesomeIcon
+                icon={faBars}
+                color='#ffd700'
+                size='3x'
+                className='hamburger-icon'
+            />
+        </div>
+    )
+}
 
 export default Sidebar;
